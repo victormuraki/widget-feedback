6faac4717470501a54047464bfc9f2bbc5f76b02
Victor Muraki <vmuraki@MKMONDIAL.local>
2022-06-09 17:24:00 -0300

    Inclusao componente, CLoseButton, widgetForm.

---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,51 @@
+import { CloseButton } from "./CloseButton";
+
+import bugImageUrl from "../assets/bug.svg"
+import ideaImageUrl from "../assets/bug.svg"
+import thoughtImageUrl from "../assets/bug.svg"
+
+const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: ''
+        }
+    },
+    IDEA: {
+        title: 'Idea',
+        image: {
+            source: ideaImageUrl,
+            alt: ''
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: ''
+        }
+    },
+
+}
+
+export function WidgetForm() {
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+                <button></button>
+
+            </div>
+
+            <footer className="text-xs text-neutral-400">
+                Feito com carinho por <a className="underline underline-offset-2" href="https://github.com/victormuraki">Victor Muraki</a> ❤️
+            </footer>
+        </div>
+    )
+}
